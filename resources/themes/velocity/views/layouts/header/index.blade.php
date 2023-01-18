5731e83598edc554527819306852a4c1a97bb7df
Marko Pejanovic <mare.sampbn@gmail.com>
2023-01-18 19:38:42 +0100

    added header links

--- resources/themes/velocity/views/layouts/header/index.blade.php
+++ resources/themes/velocity/views/layouts/header/index.blade.php
@@ -2,12 +2,20 @@
 <div class="container">
 
 
-    <div class="row remove-padding-margin velocity-divide-page">
-        <a class="left navbar-brand" href="{{ route('shop.home.index') }}" aria-label="Logo">
+    <div class="row remove-padding-margin velocity-divide-page d-flex justify-content-between align-items-center">
+        <a class="navbar-brand" href="{{ route('shop.home.index') }}" aria-label="Logo">
             <img class="logo" src="{{ core()->getCurrentChannel()->logo_url ?? asset('themes/velocity/assets/images/logo-text.png') }}" alt="" />
         </a>
+        <div class="row gap-4">
+            <a href="/" class="text-white">Homepage</a>
+            <a href="/" class="text-white ml-4">Shop</a>
+            <a href="{{ route("shop.cms.page", 'about-us') }}" class="text-white ml-4">About the label</a>
+            <a href="/" class="text-white ml-4">Behind the scenes</a>
+            <a href="{{ route("shop.cms.page", 'contact-us') }}" class="text-white ml-4">Contact us</a>
+        </div>
 
-        <div class="right searchbar">
+
+        <div class="searchbar">
             <div class="row">
                 <div class="col-lg-5 col-md-12">
                     <!-- @include('velocity::shop.layouts.particals.search-bar') -->
@@ -15,7 +23,7 @@
 
                 <div class="col-lg-7 col-md-12 vc-full-screen">
                     <div class="left-wrapper d-flex align-items-center justify-content-center">
-                        
+
 
                         {!! view_render_event('bagisto.shop.layout.header.wishlist.before') !!}
 
