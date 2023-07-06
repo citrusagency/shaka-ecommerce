68ec5048213dd9b13e7e51ca2a7e1e0b3a57761e
Marko Pejanovic <mare.sampbn@gmail.com>
2023-07-06 23:23:19 +0200

    Added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,20 +18,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Drop all tables
-//        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-//        foreach(DB::select('SHOW TABLES') as $table) {
-//            $table_array = get_object_vars($table);
-//            Schema::drop($table_array[key($table_array)]);
-//        }
-//        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        foreach(DB::select('SHOW TABLES') as $table) {
+            $table_array = get_object_vars($table);
+            Schema::drop($table_array[key($table_array)]);
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         // Run your seeders
-        $this->call(BagistoDatabaseSeeder::class);
-        $this->call(VelocityMetaDataSeeder::class);
+//        $this->call(BagistoDatabaseSeeder::class);
+//        $this->call(VelocityMetaDataSeeder::class);
 
-        // Import db.sql file to database
-//        $sql = file_get_contents(__DIR__ . '/db.sql');
+//         Import db.sql file to database
+        $sql = file_get_contents(__DIR__ . '/db.sql');
 
-//        DB::unprepared($sql);
+        DB::unprepared($sql);
     }
 }
