4f869d8c5d5068ff72da5f03e3b050ac7d53364b
Jovana99code <jovana.perisic@citrus.codes>
2024-03-21 15:36:03 +0100

    Redesign of all mail views.

--- resources/themes/default/views/emails/customer/invoice-remainder.blade.php
+++ resources/themes/default/views/emails/customer/invoice-remainder.blade.php
@@ -5,11 +5,15 @@
     </p>
 
     <p class="p-text">
-        {!! __('shop::app.mail.customer.registration.greeting') !!}
+        {!! __('shop::app.mail.invoice.reminder.your-invoice-is-overdue', ['invoice' => $invoice->increment_id, 'time' => $invoice->created_at->diffForHumans()]) !!}
     </p>
 
     <p class="p-text w-pd">
-        {{ __('shop::app.mail.customer.registration.summary') }}
+    {!! __('shop::app.mail.invoice.reminder.please-make-your-payment-as-soon-as-possible') !!}
+    </p>
+
+    <p class="p-text">
+    {!! __('shop::app.mail.invoice.reminder.if-you-ve-already-paid-just-disregard-this-email') !!}
     </p>
 
     <p class="p-text">
