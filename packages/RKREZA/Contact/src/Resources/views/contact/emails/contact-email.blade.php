ccdcd41b218cf6eb75d52bbccabf7c904d87c573
Jovana99code <jovana.perisic@citrus.codes>
2024-03-25 10:17:20 +0100

    Redesign of contact message email

--- packages/RKREZA/Contact/src/Resources/views/contact/emails/contact-email.blade.php
+++ packages/RKREZA/Contact/src/Resources/views/contact/emails/contact-email.blade.php
@@ -1,43 +1,26 @@
 @component('shop::emails.layouts.master')
 
-    <div>
-        <div style="text-align: center;">
-            <a href="{{ config('app.url') }}">
-                @include ('shop::emails.layouts.logo')
-            </a>
+    <div class="p-text w-pd" style="font-size: 19px !important;">
+        You have a new message:
+    </div>
+    <div class="message-container">
+        <div class="p-text">
+            <b>Name: </b>
+            <span>{{ $data['name'] }}</span>
         </div>
-
-        <div style="font-size:20px; color:#242424; font-weight:600; margin-bottom: 15px">
-            You have a new message
+        <div class="p-text">
+           <b>Email: </b>
+            <span>{{ $data['email'] }}</span>
         </div>
-
-        <div>
-            <div style="font-size:16px; color:#242424;">
-                <b>Name: </b>
-                <p>{{ $data['name'] }}
-                </p>
-            </div>
-
-            <div style="font-size:16px; color:#242424;">
-               <b>Email: </b>
-                <p>
-                   {{ $data['email'] }}
-               </p>
-            </div>
-
-
-            <div style="font-size:16px; color:#242424;">
-                <b>Subject: </b>
-                <p>{{ $data['subject'] }}
-                </p>
-            </div>
-
-            <div style="font-size:16px; color:#242424;">
-                <b>Message: </b>
-                <p>{{ $data['message_body'] }}
-                </p>
-            </div>
-
+        <div class="p-text">
+            <b>Subject: </b>
+            <span>{{ $data['subject'] }}</span>
         </div>
+        <div class="p-text">
+            <b>Message: </b>
+            <span>{{ $data['message_body'] }}</span>
+        </div>
+    </div>
+
 
 @endcomponent
