409848bdc2e31a2376c8d4ed2869f05ae2859ca3
Jovana99code <jovana.perisic@citrus.codes>
2024-04-12 13:09:03 +0200

    Footer links set up

--- resources/themes/velocity/views/layouts/footer/footer-links.blade.php
+++ resources/themes/velocity/views/layouts/footer/footer-links.blade.php
@@ -5,18 +5,18 @@
                 {!! file_get_contents(public_path('/images/logo.svg')) !!}
                 <p class="text-shaka-subtitle my-4">Based in Montenegro</p>
                 <div>
-                    <a href="#" class="ml-3" target="_blank"><img style="height: 25px !important;" src="{{ asset('images/instagram-logo.svg') }}" alt=""></a>
-                    <a href="#" class="ml-5" target="_blank"><img style="height: 25px !important;" src="{{ asset('images/facebook-logo.svg') }}" alt=""></a>
+                    <a href="https://www.instagram.com/katarinazlajic/?hl=en" class="ml-3" target="_blank"><img style="height: 25px !important;" src="{{ asset('images/instagram-logo.svg') }}" alt=""></a>
+                    <a href="https://www.facebook.com/katarinazlajicaccessories/?locale=sr_RS" class="ml-5" target="_blank"><img style="height: 25px !important;" src="{{ asset('images/facebook-logo.svg') }}" alt=""></a>
                 </div>
             </div>
             <hr style="height:1px; background-color:rgba(218,218,218,0.3); margin:30px 0; border-radius: 40%;"/>
             <div class="col">
                 <div class="row justify-content-between">
                     <div class="col">
-                        <div><a href="#" class="footer-links footer-text">Home</a></div>
-                        <div class="mt-3"><a href="#" class="footer-links footer-text">Shop</a></div>
-                        <div class="mt-3"><a href="#" class="footer-links footer-text">About</a></div>
-                        <div class="mt-3"><a href="#" class="footer-links footer-text">Contact</a></div>
+                        <div><a href="{{route('shop.home.index')}}" class="footer-links footer-text">Home</a></div>
+                        <div class="mt-3"><a href="{{route('shop.getAllProducts')}}" class="footer-links footer-text">Shop</a></div>
+                        <div class="mt-3"><a href="{{route('shop.about')}}" class="footer-links footer-text">About</a></div>
+                        <div class="mt-3"><a href="{{route('shop.contact.index')}}" class="footer-links footer-text">Contact</a></div>
                     </div>
                     <div class="col">
                         <div><a href="{{ route('shop.terms-conditions') }}"  class="footer-links footer-text">Terms and Conditions</a></div>
@@ -33,7 +33,7 @@
             </div>
             <div class="col-6">
                 <span class="float-right made-by footer-text text-shaka-subtitle">
-                    Made by <span class="citrus-codes footer-text">Citrus Codes</span>
+                    Made by <a href="https://www.citrus.codes/" target="_blank" class="citrus-codes footer-text">Citrus Codes</a>
                 </span>
             </div>
         </div>
