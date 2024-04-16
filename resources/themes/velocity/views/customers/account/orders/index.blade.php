a630313adc4a7b3f3d9474961c700072b04c26d9
Ilija019 <ilija.radojicic019@gmail.com>
2024-04-16 15:21:00 +0200

    wip profile > orders table responsiveness

--- resources/themes/velocity/views/customers/account/orders/index.blade.php
+++ resources/themes/velocity/views/customers/account/orders/index.blade.php
@@ -6,18 +6,28 @@
 
 @section('page-detail-wrapper')
     <div class="profile-content-title">
-            {{ __('shop::app.customer.account.order.index.title') }}
+        {{ __('shop::app.customer.account.order.index.title') }}
     </div>
 
     {!! view_render_event('bagisto.shop.customers.account.orders.list.before') !!}
 
-        <div class="">
-            <div class="">
+<div class="account-items-list">
+
+    <div class="account-table-content">
+        <div class="table">
+            <div class="grid-container">
 
                 <datagrid-plus src="{{ route('customer.orders.index') }}"></datagrid-plus>
 
             </div>
         </div>
+    </div>
+</div>
+
+
 
     {!! view_render_event('bagisto.shop.customers.account.orders.list.after') !!}
 @endsection
+@push('scripts')
+    @include('admin::export.export', ['gridName' => app('Webkul\Admin\DataGrids\OrderDataGrid')])
+@endpush
